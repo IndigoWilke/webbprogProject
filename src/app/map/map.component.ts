b6588409cf73bff36d37f31006fcb57ebd5f6725
IndigoWilke <filip.wilke@hotmail.se>
2023-10-10 10:43:14 +0200

    moved map to malmö and fixed icons

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -7,18 +7,26 @@ import * as L from 'leaflet';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
+  icon = {
+    icon: L.icon({
+      iconSize: [ 25, 41 ],
+      iconAnchor: [ 13, 0 ],
+      iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+      shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png'
+   })
+  };
   constructor() { }
 
   ngOnInit(): void {
-    const map = L.map('map').setView([51.505, -0.09], 13);
+    const map = L.map('map').setView([55.61, 13.02], 13);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    L.marker([51.5, -0.09]).addTo(map)
+    L.marker([55.61, 13.02], this.icon).addTo(map)
       .bindPopup('A sample marker');
-    L.marker([51.5, -0.08]).addTo(map)
-      .bindPopup('A sample marker but again');
+    L.marker([55.61, 13.04], this.icon).addTo(map)
+      .bindPopup('A sample marker but to the right');
   }
 }
