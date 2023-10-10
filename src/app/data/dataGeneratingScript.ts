5ad1e05ebc72deb271c7fa4637594c23313e68f5
AhmadGhaemi <ahmad.ghaemi@hotmail.com>
2023-10-10 18:24:53 +0200

    fixed some issues with the data. NOTE: see info and warning on last commit message

--- src/app/data/dataGeneratingScript.ts
+++ src/app/data/dataGeneratingScript.ts
@@ -1,4 +1,4 @@
-import * as fs from 'fs'; //for writing to file 
+//import * as fs from 'fs'; //for writing to file 
 import rawData from "./rawData";
 
 //filtrerar rådatan lite:
@@ -22,7 +22,7 @@ enum GalleriTyp {galleri = 'Gallerier och utställningsrum', popup = 'Popup'}
 
 enum genre {modern = 'Modern konst', klassisk = 'Klassisk konst', performance = 'Performance-konst', feministisk = 'Feministisk konst', ljud = 'Ljudkonst', övrigt = 'Övrigt'};
 function getRandomGenre(): genre {
-  return Object.values(genre)[getRandomInt((Object.values.length))];
+  return Object.values(genre)[getRandomInt((Object.values(genre).length))];
 }
 
 type öppetTider = {öppnar: number, stänger: number};
@@ -44,24 +44,24 @@ function getRandomOpeningHours(): öppetTider {
 
 //the one type to rule them all:
 type utställare = {
-  namn: String,
-  koordinater: any,
-  galleriTyp: GalleriTyp, 
-  öppetTider: öppetTider,
+  name: String,
+  coordinates: any,
+  galleryType: GalleriTyp, 
+  openingHours: öppetTider,
   genre: genre,
-  serverarMat: boolean,
-  serveringsTillstånd: boolean
+  servesFood: boolean,
+  servesAlcoholicBev: boolean
 }
 
 function skapaUtställare(namn: String, koordinater: any, galleriTyp: GalleriTyp, öppetTider: öppetTider, genre: genre, serverarMat: boolean, serveringsTillstånd: boolean): utställare { 
     return { 
-      namn: namn,
-      koordinater: koordinater,
-      galleriTyp: galleriTyp, 
-      öppetTider: öppetTider,
+      name: namn,
+      coordinates: koordinater,
+      galleryType: galleriTyp, 
+      openingHours: öppetTider,
       genre: genre,
-      serverarMat: serverarMat,
-      serveringsTillstånd: serveringsTillstånd
+      servesFood: serverarMat,
+      servesAlcoholicBev: serveringsTillstånd
     }
 }
 
@@ -86,7 +86,7 @@ gallerierOchUtställningsRum.forEach((utställare: any[]) => {
 popUp.forEach((utställare: any[]) => {
   const namn = utställare[5][0][0];
   const koordinater = utställare[4][4];
-  const galleriTyp = GalleriTyp.galleri;
+  const galleriTyp = GalleriTyp.popup;
   const öppetTider = getRandomOpeningHours();
   const genre = getRandomGenre();
   const serverarMat = getRandomBoolean();
@@ -96,11 +96,9 @@ popUp.forEach((utställare: any[]) => {
 });
 
 const allaUtställare = galleriUtställare.concat(popUpUtställare)
-const jsonizedData = JSON.stringify(allaUtställare, null, 2)
-console.log(jsonizedData);
-const filename = './data.ts'
-fs.writeFileSync(filename, jsonizedData, 'utf-8');
+console.log(allaUtställare);
+//const filename = './src/app/data/data.ts'  //Don't write to file, since we want to create a variabl and export it. Just console.log and copy-paste for now.
+//fs.writeFileSync(filename, jsonizedData, 'utf-8'); //See line above.
 //^Doesn't work since we are in Angular probably. Need to move to server side but im too tired now, will just manually copy over the data in the meantime.
 
 export default allaUtställare;
-
